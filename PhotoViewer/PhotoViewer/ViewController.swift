13e644d21bb6c94dda394ddc1f07be667786a49b
Illya Gordiyenko <illyagordy@gmail.com>
2018-04-07 17:32:10 -0400

    Hello world project setup

---
+++ PhotoViewer/PhotoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by Illya Gordiyenko on 2018-04-07.
+//  Copyright © 2018 Illya Gordiyenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
